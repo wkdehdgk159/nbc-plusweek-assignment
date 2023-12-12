34c515ce675532ad9f5b42b69d078ff986e75ad3
Dongha <wkdehdgk159@gmail.com>
2023-12-12 12:32:35 +0900

    initial commit

---
+++ src/main/java/com/example/nbcplusweekassignment/NbcPlusweekAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbcplusweekassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcPlusweekAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbcPlusweekAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nbcplusweekassignment/NbcPlusweekAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbcplusweekassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcPlusweekAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
